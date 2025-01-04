a15c94aa4fadd7582c21f440bea337e6f8c7e862
RajDaBest <imagination12344321.com@gmail.com>
2025-01-04 16:33:05 +0530

    minor changes

--- LED/source/main.c
+++ LED/source/main.c
@@ -8,24 +8,16 @@
 
 int main(void)
 {
-    // Enable GPIOA clock
     RCC->AHB1ENR |= GPIOA_EN;
 
-    // Clear the mode bits for PA5
-    GPIOA->MODER &= ~(3U << 10); // Clear bits 10-11
-    // Set PA5 as output
-    GPIOA->MODER |= (1U << 10); // Set bit 10
+    GPIOA->MODER &= ~(3U << 10);
+    GPIOA->MODER |= (1U << 10);
 
-    // Make sure pin is push-pull (default, but let's be explicit)
     GPIOA->OTYPER &= ~(1U << 5);
 
-    // Set to low speed
     GPIOA->OSPEEDR &= ~(3U << 10);
 
-    // No pull-up or pull-down
     GPIOA->PUPDR &= ~(3U << 10);
-
-    // void *ptr = malloc(10);
 
     while (1)
     {
