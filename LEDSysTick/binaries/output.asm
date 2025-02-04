571bdb4e361791978b7e4692901d53c56cb98202
neofytr <imagination12344321.com@gmail.com>
2025-02-05 00:02:57 +0530

    worked with systick

---
+++ LEDSysTick/binaries/output.asm
@@ -0,0 +1,184 @@
+
+binaries/output.elf:     file format elf32-littlearm
+
+
+Disassembly of section .isr_vector:
+
+08000000 <vectors>:
+ 8000000:	20018000 	andcs	r8, r1, r0
+ 8000004:	080001a5 	stmdaeq	r0, {r0, r2, r5, r7, r8}
+ 8000008:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 800000c:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 8000010:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 8000014:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 8000018:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+	...
+ 800002c:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 8000030:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 8000034:	00000000 	andeq	r0, r0, r0
+ 8000038:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 800003c:	0800017d 	stmdaeq	r0, {r0, r2, r3, r4, r5, r6, r8}
+ 8000040:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 8000044:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 8000048:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 800004c:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 8000050:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 8000054:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 8000058:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 800005c:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 8000060:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 8000064:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 8000068:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 800006c:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 8000070:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 8000074:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 8000078:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 800007c:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 8000080:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 8000084:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 8000088:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 800008c:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 8000090:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 8000094:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 8000098:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 800009c:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 80000a0:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 80000a4:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 80000a8:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 80000ac:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 80000b0:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 80000b4:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 80000b8:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 80000bc:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 80000c0:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 80000c4:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 80000c8:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 80000cc:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 80000d0:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 80000d4:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 80000d8:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 80000dc:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 80000e0:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 80000e4:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 80000e8:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 80000ec:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 80000f0:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 80000f4:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 80000f8:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 80000fc:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 8000100:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 8000104:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 8000108:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 800010c:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 8000110:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 8000114:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 8000118:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+ 800011c:	0800018d 	stmdaeq	r0, {r0, r2, r3, r7, r8}
+	...
+
+Disassembly of section .text:
+
+08000150 <delay>:
+ 8000150:	bf00      	nop
+ 8000152:	e7fd      	b.n	8000150 <delay>
+
+08000154 <setup_systick>:
+ 8000154:	f04f 23e0 	mov.w	r3, #3758153728	@ 0xe000e000
+ 8000158:	691a      	ldr	r2, [r3, #16]
+ 800015a:	f042 0201 	orr.w	r2, r2, #1
+ 800015e:	611a      	str	r2, [r3, #16]
+ 8000160:	691a      	ldr	r2, [r3, #16]
+ 8000162:	f042 0204 	orr.w	r2, r2, #4
+ 8000166:	611a      	str	r2, [r3, #16]
+ 8000168:	691a      	ldr	r2, [r3, #16]
+ 800016a:	f042 0202 	orr.w	r2, r2, #2
+ 800016e:	611a      	str	r2, [r3, #16]
+ 8000170:	4a01      	ldr	r2, [pc, #4]	@ (8000178 <setup_systick+0x24>)
+ 8000172:	615a      	str	r2, [r3, #20]
+ 8000174:	4770      	bx	lr
+ 8000176:	bf00      	nop
+ 8000178:	00f423ff 	ldrsheq	r2, [r4], #63	@ 0x3f	@ <UNPREDICTABLE>
+
+0800017c <SysTick_handler>:
+ 800017c:	4a02      	ldr	r2, [pc, #8]	@ (8000188 <SysTick_handler+0xc>)
+ 800017e:	6953      	ldr	r3, [r2, #20]
+ 8000180:	f083 0320 	eor.w	r3, r3, #32
+ 8000184:	6153      	str	r3, [r2, #20]
+ 8000186:	4770      	bx	lr
+ 8000188:	40020000 	andmi	r0, r2, r0
+
+0800018c <default_handler>:
+ 800018c:	f000 b832 	b.w	80001f4 <system_reset>
+
+08000190 <enable_fpu>:
+ 8000190:	4a03      	ldr	r2, [pc, #12]	@ (80001a0 <enable_fpu+0x10>)
+ 8000192:	f8d2 3088 	ldr.w	r3, [r2, #136]	@ 0x88
+ 8000196:	f443 0370 	orr.w	r3, r3, #15728640	@ 0xf00000
+ 800019a:	f8c2 3088 	str.w	r3, [r2, #136]	@ 0x88
+ 800019e:	4770      	bx	lr
+ 80001a0:	e000ed00 	and	lr, r0, r0, lsl #26
+
+080001a4 <reset_handler>:
+ 80001a4:	b510      	push	{r4, lr}
+ 80001a6:	490e      	ldr	r1, [pc, #56]	@ (80001e0 <reset_handler+0x3c>)
+ 80001a8:	4b0e      	ldr	r3, [pc, #56]	@ (80001e4 <reset_handler+0x40>)
+ 80001aa:	480f      	ldr	r0, [pc, #60]	@ (80001e8 <reset_handler+0x44>)
+ 80001ac:	1a5b      	subs	r3, r3, r1
+ 80001ae:	2200      	movs	r2, #0
+ 80001b0:	429a      	cmp	r2, r3
+ 80001b2:	d10b      	bne.n	80001cc <reset_handler+0x28>
+ 80001b4:	4a0d      	ldr	r2, [pc, #52]	@ (80001ec <reset_handler+0x48>)
+ 80001b6:	4b0e      	ldr	r3, [pc, #56]	@ (80001f0 <reset_handler+0x4c>)
+ 80001b8:	1ad1      	subs	r1, r2, r3
+ 80001ba:	2200      	movs	r2, #0
+ 80001bc:	4610      	mov	r0, r2
+ 80001be:	428a      	cmp	r2, r1
+ 80001c0:	d10a      	bne.n	80001d8 <reset_handler+0x34>
+ 80001c2:	f7ff ffe5 	bl	8000190 <enable_fpu>
+ 80001c6:	f000 f827 	bl	8000218 <main>
+ 80001ca:	e7fe      	b.n	80001ca <reset_handler+0x26>
+ 80001cc:	f810 4b01 	ldrb.w	r4, [r0], #1
+ 80001d0:	f801 4b01 	strb.w	r4, [r1], #1
+ 80001d4:	3201      	adds	r2, #1
+ 80001d6:	e7eb      	b.n	80001b0 <reset_handler+0xc>
+ 80001d8:	f803 0b01 	strb.w	r0, [r3], #1
+ 80001dc:	3201      	adds	r2, #1
+ 80001de:	e7ee      	b.n	80001be <reset_handler+0x1a>
+ 80001e0:	20000000 	andcs	r0, r0, r0
+ 80001e4:	20000000 	andcs	r0, r0, r0
+ 80001e8:	08000244 	stmdaeq	r0, {r2, r6, r9}
+ 80001ec:	20000000 	andcs	r0, r0, r0
+ 80001f0:	20000000 	andcs	r0, r0, r0
+
+080001f4 <system_reset>:
+ 80001f4:	f3bf 8f4f 	dsb	sy
+ 80001f8:	4905      	ldr	r1, [pc, #20]	@ (8000210 <system_reset+0x1c>)
+ 80001fa:	4b06      	ldr	r3, [pc, #24]	@ (8000214 <system_reset+0x20>)
+ 80001fc:	68ca      	ldr	r2, [r1, #12]
+ 80001fe:	f402 62e0 	and.w	r2, r2, #1792	@ 0x700
+ 8000202:	4313      	orrs	r3, r2
+ 8000204:	60cb      	str	r3, [r1, #12]
+ 8000206:	f3bf 8f4f 	dsb	sy
+ 800020a:	bf00      	nop
+ 800020c:	e7fd      	b.n	800020a <system_reset+0x16>
+ 800020e:	bf00      	nop
+ 8000210:	e000ed00 	and	lr, r0, r0, lsl #26
+ 8000214:	05fa0004 	ldrbeq	r0, [sl, #4]!
+
+08000218 <main>:
+ 8000218:	4a08      	ldr	r2, [pc, #32]	@ (800023c <main+0x24>)
+ 800021a:	b508      	push	{r3, lr}
+ 800021c:	6b13      	ldr	r3, [r2, #48]	@ 0x30
+ 800021e:	f043 0301 	orr.w	r3, r3, #1
+ 8000222:	6313      	str	r3, [r2, #48]	@ 0x30
+ 8000224:	4b06      	ldr	r3, [pc, #24]	@ (8000240 <main+0x28>)
+ 8000226:	681a      	ldr	r2, [r3, #0]
+ 8000228:	f442 6280 	orr.w	r2, r2, #1024	@ 0x400
+ 800022c:	601a      	str	r2, [r3, #0]
+ 800022e:	681a      	ldr	r2, [r3, #0]
+ 8000230:	f422 6200 	bic.w	r2, r2, #2048	@ 0x800
+ 8000234:	601a      	str	r2, [r3, #0]
+ 8000236:	f7ff ff8d 	bl	8000154 <setup_systick>
+ 800023a:	e7fe      	b.n	800023a <main+0x22>
+ 800023c:	40023800 	andmi	r3, r2, r0, lsl #16
+ 8000240:	40020000 	andmi	r0, r2, r0
---
+++ LEDSysTick/source/main.c
@@ -0,0 +1,76 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include "STM32F401.h"
+#include "core_cm4.h"
+
+/*
+ * Hardware Configuration:
+ * LED is connected to PA5 (Port A, Pin 5)
+ *
+ * Clock Configuration:
+ * GPIOA is connected to the AHB1 bus
+ * Clock must be enabled via RCC->AHB1ENR before GPIOA can be used
+ *
+ * GPIO Configuration:
+ * Each GPIO pin has 2 mode bits in the MODER register
+ * For PA5, these are bits 10-11 (2 bits per pin, so pin 5 * 2)
+ * Mode 01 = General Purpose Output
+ */
+
+// Clock enable bit for GPIOA in RCC_AHB1ENR register
+#define GPIOA_EN (0)
+
+// Pin 5 definitions
+#define PIN5 (5) // Bit 5 in ODR register for PA5
+// For output pin: MODER is 01
+
+// Bit manipulation macros
+#define SET_BIT(reg, bit) ((reg) |= ((1UL) << (bit)))
+#define CLEAR_BIT(reg, bit) ((reg) &= ~((1UL) << (bit)))
+#define TOGGLE_BIT(reg, bit) ((reg) ^= ((1UL) << (bit)))
+
+void delay(void);
+void setup_systick(void);
+
+void delay(void)
+{
+    while (true)
+    {
+        __asm("nop");
+    }
+}
+
+#define SYS_CLOCK 16000000 // 16MHz is the default system clock without any configuration
+
+void setup_systick(void)
+{
+#define COUNTER_ENABLE (0)
+    SET_BIT(SysTick->CTRL, COUNTER_ENABLE); // enable counter
+#define CLOCK_SOURCE (2)
+    SET_BIT(SysTick->CTRL, CLOCK_SOURCE); // select the processor clock
+#define SYSTICK_INTERRUPT (1)
+    SET_BIT(SysTick->CTRL, SYSTICK_INTERRUPT); // enables the system interrupt request
+
+    SysTick->LOAD = 0x00FFFFFFU & (unsigned int)(1 * SYS_CLOCK - 1); // timer with a period of SYS_CLOCK processor clock cycles
+}
+
+void SysTick_handler(void)
+{
+    GPIOA->ODR ^= 1UL << PIN5;
+}
+
+int main(void)
+{
+    // 1. Enable clock access to GPIOA via AHB1 bus
+    SET_BIT(RCC->AHB1ENR, GPIOA_EN);
+
+    // 2. Configure PA5 as general purpose output (01)
+    // First clear both mode bits, then set the lower bit
+    SET_BIT(GPIOA->MODER, PIN5 * 2);
+    CLEAR_BIT(GPIOA->MODER, PIN5 * 2 + 1);
+    setup_systick();
+
+    while (true)
+        ;
+    return 0;
+}
